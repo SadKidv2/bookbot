d3d1b619e33dd2d9e13927f482def440961d480a
SadKidv2 <sajidislam30255@gmail.com>
2025-08-26 18:27:50 +0000

    Complete file reading

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    book_text = get_book_text("frankenstein.txt")
+    book_text = get_book_text("books/frankenstein.txt")
     print(book_text)
 
 if __name__ == "__main__":
